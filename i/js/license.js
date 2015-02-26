0f5148035818b7e2a5774314aabc8f0e04839310
Paul Irish <paul.irish@gmail.com>
2015-02-25 17:50:45 -0800

    sourcemap for builder and license preamble.

---
+++ i/js/license.js
@@ -0,0 +1,8 @@
+/**
+ * Modulizr
+ * Modernizr Modular Build Tool
+ * Very simple tool for including or excluding tests
+ *
+ * @Author  Alex Sexton - AlexSexton@gmail.com
+ * @License Dual MIT and WTFPL
+ */
